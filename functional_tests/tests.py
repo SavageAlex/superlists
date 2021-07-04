d07a339abbdd02bb34d7401b009ed512f54f1a96
SavageAlex <atalaver@gmail.com>
2021-07-04 21:05:53 +0200

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -109,7 +109,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis gets his own umique URL
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
-        self.asserNotEqual(francis_list_url, edith_list_url)
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
