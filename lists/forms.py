696469b0b18b85459ae1176d69391d79a8f8212c
SavageAlex <atalaver@gmail.com>
2021-10-20 17:57:03 +0200

    add list owners via forms. more isolated tests

--- lists/forms.py
+++ lists/forms.py
@@ -1,15 +1,11 @@
 from django.core.exceptions import ValidationError
 from django import forms
-from lists.models import Item
+from lists.models import Item, List
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
-
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
     
     class Meta:
         model = Item
@@ -24,14 +20,19 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
 
+class NewListForm(ItemForm):
+    
+    def save(self, owner):
+        if owner.is_authenticated:
+            return List.create_new(first_item_text=self.cleaned_data['text'], owner=owner)
+        else:
+            return List.create_new(first_item_text=self.cleaned_data['text'])
+
 class ExistingListItemForm(ItemForm):
 
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.instance.list = for_list
-
-    def save(self):
-        return forms.models.ModelForm.save(self)
 
     def validate_unique(self):
         try:
